cea9ac65a2ffeea345dee4cf9855b3214b9e47c9
JFGEER83 <jfgeerinckx@mac.com>
2024-12-12 18:08:39 +0100

    add js sticky menu

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+window.addEventListener('scroll', function(){
+  let header = document.querySelector("header")
+  if(this.window.scrollY>250){
+    header.classList.add("scroll-active")
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  else{
+    header.classList.remove("scroll-active");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+});  
